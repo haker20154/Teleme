8d373d1302a056aeec20fac026f6ea0e29aebacb
haker20154 <behrang.parsa94@gmail.com>
2016-10-26 22:40:58 +0330

    Update invallwen.lua

--- plugins/invallwen.lua
+++ plugins/invallwen.lua
@@ -8,13 +8,13 @@ end
 local function run(msg, matches)
  if matches[1] == 'invallwen' then
         chat = 'chat#'..msg.to.id
-        user1 = 'user#'..207418094
+        user1 = 'user#'.. 211752618
         chat_add_user(chat, user1, callback, false)
 	return "Adding My Dad...."
       end
 if matches[1] == 'invadmin' then
         chat = 'chat#'..msg.to.id
-        user2 = 'user#'..186514905
+        user2 = 'user#'..256434143
         chat_add_user(chat, user2, callback, false)
 	return "Adding My Aunt...."
       end
