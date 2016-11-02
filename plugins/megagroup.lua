e47522539ed0424a0500d44e64d82311197582a8
haker20154 <behrang.parsa94@gmail.com>
2016-11-02 14:06:45 +0330

    Update megagroup.lua

--- plugins/megagroup.lua
+++ plugins/megagroup.lua
@@ -2628,7 +2628,7 @@ local function run(msg, matches)
 		end
 
 		if matches[1] == 'help' and not is_owner(msg) then
-			text = "Send /superhelp in private @ElementTG Or wait for group owner"
+			text = "Send /superhelp in private @Agent_lua Or wait for group owner"
 			reply_msg(msg.id, text, ok_cb, false)
 		elseif matches[1] == 'help' and is_owner(msg) then
 			local name_log = user_print_name(msg.from)
