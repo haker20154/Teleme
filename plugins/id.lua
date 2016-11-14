98ea4e7f92c87016fd5dc729f989b3331f156b46
haker20154 <behrang.parsa94@gmail.com>
2016-11-14 19:55:09 +0330

    Update id.lua

--- plugins/id.lua
+++ plugins/id.lua
@@ -1,19 +1,15 @@
 do
-
+— created by Telegram.me/boy_virtual
+— کپی بدون ذکر منبع حرام است!
 function run(msg, matches)
-local reply_id = msg['id']
-
-local info = #شناسه گروه : "..msg.from.id.."\n#نام گروه : "..msg.to.title.."\n#نام شما : "..(msg.from.first_name or '').."\n#نام اول : "..(msg.from.first_name or '').."\n#نام آخر : "..(msg.from.last_name or '').."\n#آیدی : "..msg.from.id.."\n#یوزرنیم : @"..(msg.from.username or '').."\n#شماره تلفن : +"..(msg.from.phone or '')
-
-reply_msg(reply_id, info, ok_cb, false)
+  return "#شناسه گروه : "..msg.from.id.."\n#نام گروه : "..msg.to.title.."\n#نام شما : "..(msg.from.first_name or '').."\n#نام اول : "..(msg.from.first_name or '').."\n#نام آخر : "..(msg.from.last_name or '').."\n#آیدی : "..msg.from.id.."\n#یوزرنیم : @"..(msg.from.username or '').."\n#شماره تلفن : +"..(msg.from.phone or '')
 end
-
 return {
-patterns = {
-"^[!/#]([Ii][Dd])"
-
-},
-run = run
+  description = "", 
+  usage = "",
+  patterns = {
+    "^[!/#]id$",
+  },
+  run = run
 }
-
 end
